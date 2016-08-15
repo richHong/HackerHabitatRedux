d6149065c8c57c5513b6b04547ca125ea1f976c0
Richard Hong <rich.hong@gmail.com>
2016-08-14 20:27:41 -0700

    worked on single listing component

--- public/components/singlelisting.js
+++ public/components/singlelisting.js
@@ -27,34 +27,34 @@ class SingleListing extends React.Component {
         <div className="houseInfo">
 
   				<span className='houseName'>
-            <b>{ this.props.listing ? this.props.listing.house_name : this.state.house_name }</b>
+            <b>{ this.props.listing.house_name ? this.props.listing.house_name : this.state.house_name }</b>
           </span><br/>
 
-          <i>{ this.props.listing ? this.props.listing.heading : this.state.heading }</i><br/><br/>
+          <i>{ this.props.listing.heading ? this.props.listing.heading : this.state.heading }</i><br/><br/>
 
           <b><i>Location:</i></b><br/>
-          { this.props.listing ? this.props.listing.street_add : this.state.street_add }<br/>
-          { this.props.listing ? this.props.listing.city : this.state.city },{ this.props.listing ? this.props.listing.state : this.state.state } { this.props.listing ? this.props.listing.zipcode : this.state.zipcode }<br/><br/>
+          { this.props.listing.street_add ? this.props.listing.street_add : this.state.street_add }<br/>
+          { this.props.listing.city ? this.props.listing.city : this.state.city },{ this.props.listing.state ? this.props.listing.state : this.state.state } { this.props.listing.zipcode ? this.props.listing.zipcode : this.state.zipcode }<br/><br/>
 
-          <b><i>Price:</i></b> ${ this.props.listing ? this.props.listing.price : this.state.price } per night<br /><br/>
+          <b><i>Price:</i></b> ${ this.props.listing.price ? this.props.listing.price : this.state.price } per night<br /><br/>
 
-          <b><i>Vacancies:</i></b> { this.props.listing ? this.props.listing.vacancies : this.state.vacancies } <br /><br />
+          <b><i>Vacancies:</i></b> { this.props.listing.vacancies ? this.props.listing.vacancies : this.state.vacancies } <br /><br />
 
-          <b><i>Dates Available:</i></b> { this.props.listing ? this.props.listing.dates_avail : this.state.dates_avail } <br /><br />
+          <b><i>Dates Available:</i></b> { this.props.listing.dates_avail ? this.props.listing.dates_avail : this.state.dates_avail } <br /><br />
 
-          <b><i>House Interests:</i></b> { this.props.listing ? this.props.listing.house_interests.split(',').map((interest, i) => <span key={ i } >{ interest }, </span>) : this.state.house_interests.split(',').map((interest, i) => <span key={ i } >{ interest }, </span>)  }<br /><br />
+          <b><i>House Interests:</i></b> { this.props.listing.house_interests ? this.props.listing.house_interests.split(',').map((interest, i) => <span key={ i } >{ interest }, </span>) : this.state.house_interests.split(',').map((interest, i) => <span key={ i } >{ interest }, </span>)  }<br /><br />
 
-          <b><i>House Mission:</i></b> { this.props.listing ? this.props.listing.house_mission : this.state.house_mission } <br /><br />
+          <b><i>House Mission:</i></b> { this.props.listing.house_mission ? this.props.listing.house_mission : this.state.house_mission } <br /><br />
 
-          <b><i>House Rules:</i></b> { this.props.listing ? this.props.listing.house_rules : this.state.house_rules } <br /><br />
+          <b><i>House Rules:</i></b> { this.props.listing.house_rules ? this.props.listing.house_rules : this.state.house_rules } <br /><br />
 
-          <b><i>Amenities:</i></b> { this.props.listing ? this.props.listing.amenities.split(',').map((amenity, i) => <span key={ i } >{ amenity }, </span>) : this.state.amenities.split(',').map((amenity, i) => <span key={ i } >{ amenity }, </span>) } <br /><br />
+          <b><i>Amenities:</i></b> { this.props.listing.amenities ? this.props.listing.amenities.split(',').map((amenity, i) => <span key={ i } >{ amenity }, </span>) : this.state.amenities.split(',').map((amenity, i) => <span key={ i } >{ amenity }, </span>) } <br /><br />
 
-          <b><i>Primary Member:</i></b> { this.props.listing ? this.props.listing.primary_member : this.state.primary_member } <br /><br />
+          <b><i>Primary Member:</i></b> { this.props.listing.primary_member ? this.props.listing.primary_member : this.state.primary_member } <br /><br />
 
           <form>
             <h4 className="contactHouse">
-              <b>Contact { this.props.listing ? this.props.listing.house_name : this.state.house_name }:</b>
+              <b>Contact { this.props.listing.house_name ? this.props.listing.house_name : this.state.house_name }:</b>
             </h4>
             <label>Your Message:</label>
             <br />
@@ -87,22 +87,22 @@ class SingleListing extends React.Component {
       let message = this.message.value
       let userID = window.localStorage.getItem('userID');
 
-      fetch('http://localhost:3001/v1/users/?id=' + userID)
+      fetch('https://lit-harbor-15852.herokuapp.com/v1/users/' + userID)
       .then(response => response.json())
       .then(json => {
-        if(json.data[0]){
-          username = json.data[0].username
-          sender = json.data[0].email
+        if(json){
+          username = json.username
+          sender = json.email
         } else {
           alert("You Must Be Signed in to send a message")
         }
-        receiver = this.props.listing.user.email
-        if(this.props.listing.house_name){
-          listing = this.props.listing.house_name
+        receiver = this.props.listing.user.email || this.state.user.email
+        if(this.props.listing.house_name || this.state.house_name){
+          listing = this.props.listing.house_name || this.state.house_name
         }
       })
       .then(() => { 
-        fetch('/email', {
+        fetch('https://lit-harbor-15852.herokuapp.com/email', {
           method: 'POST',
           headers: {'content-type': 'application/json'},
           body: JSON.stringify({personalizations: [{to: [{email: receiver}]}],from: {email: sender},subject: username + " is interested in " + listing + " on Hacker Habitat" ,content: [{type: "text/plain", value: message}]})
