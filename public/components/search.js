8d231c4e81102fda661c25cdff842a15b43a1334
Richard Hong <rich.hong@gmail.com>
2016-08-22 15:32:00 -0700

    code clean up

--- public/components/search.js
+++ public/components/search.js
@@ -5,32 +5,34 @@ import { getHouseAction }                                                   from
 import { Router, Route, hashHistory, browserHistory, Link }                 from 'react-router';
 
 class SearchBar extends Component {
-	render(){
-		return (
-			<div className='searchDiv'>
+	onSubmit(e){
+    e.preventDefault();
+
+		var searchable = this.search.value.replace(" ", "+").toLowerCase();
+    
+		fetch('/v1/listings/city/:'+searchable)
+    	.then(response => response.json())
+    	.then(json => {
+        this.props.getHouseAction(json);
+        hashHistory.push('results');
+      });
+	}
+  render(){
+    return (
+      <div className='searchDiv'>
         <form onSubmit={ e => this.onSubmit(e) }>
-  				<input 
+          <input 
             placeholder='Search by City' 
             ref={ (input) => this.search = input } 
             className='searchbox' />
-      		<img 
+          <img 
             onClick={this.onSubmit.bind(this)} 
             className='magnify searchbutton'
             src='https://cdn2.iconfinder.com/data/icons/picons-basic-1/57/basic1-016_search_zoom_find-512.png'/>
         </form>
       </div>
     )
-	}
-	onSubmit(e){
-    e.preventDefault();
-		var searchable = this.search.value.replace(" ", "+").toLowerCase();;
-		fetch('/v1/listings/city/:'+searchable)
-    	.then(response => response.json())
-    	.then(json => {
-        this.props.getHouseAction(json);
-        hashHistory.push('results');
-      })
-	}
+  }
 }
 function mapDispatchToProps(dispatch) {
 	return bindActionCreators({ getHouseAction: getHouseAction }, dispatch)
