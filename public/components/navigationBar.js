efd1ebccf69d8dcf871e08802706be37a220c76b
Richard Hong <rich.hong@gmail.com>
2016-08-22 15:24:02 -0700

    code clean up

--- public/components/navigationBar.js
+++ public/components/navigationBar.js
@@ -1,6 +1,6 @@
 import React, { Component }     from 'react';
 import { Link }  								from 'react-router';
-import SearchBar 								from './searchBar';
+import SearchBar 								from './searchBar.js';
 
 let loggedIn = function() {
   return !!localStorage.token;
