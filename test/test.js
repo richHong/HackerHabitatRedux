c9c778af1d496c0822e3db00e45e7854e1c2cb13
Richard Hong <rich.hong@gmail.com>
2016-08-24 13:32:44 -0700

    added test for single listing

--- test/test.js
+++ test/test.js
@@ -2,12 +2,21 @@ import expect from 'expect';
 import * as actions from '../public/actions/houseActions';
 
 describe('actions', () => {
-  it('should create an action to add a todo', () => {
-    const listings = 'listing test';
-    const expectedAction = {
+  it('should create an action to get listings', () => {
+    const listings = 'listings test';
+    const expectedListingsAction = {
       type: 'GET_LISTINGS',
       listings
     };
-    expect(actions.getHouseAction(listings)).toEqual(expectedAction);
+    expect(actions.getHouseAction(listings)).toEqual(expectedListingsAction);
+  });
+
+  it('should create an action to get a single listing', () => {
+    const house = 'house test';
+    const expectedHouseAction = {
+      type: 'SINGLE_LISTING',
+      house
+    };
+    expect(actions.singleListingAction(house)).toEqual(expectedHouseAction);
   });
 });
