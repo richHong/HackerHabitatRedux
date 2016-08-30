1fbd13249f4a1c598e3b1e52bd5eb702b77217bb
Richard Hong <rich.hong@gmail.com>
2016-08-29 19:46:25 -0700

    fixed listing refresh carousel bug

--- public/components/carousel.js
+++ public/components/carousel.js
@@ -5,12 +5,12 @@ import { connect }                               from 'react-redux';
 class MyCarousel extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {images:[]};
   }
   componentWillMount(){
     let images = [];
 
-    if (this.props.listing !== null){
+    if (this.props.listing){
       if(this.props.listing.pic1){
         images.push(this.props.listing.pic1);
       }
@@ -26,14 +26,14 @@ class MyCarousel extends Component {
       if(this.props.listing.pic5){
         images.push(this.props.listing.pic5);
       }
-      if(!this.props.listing.pic1){
-        images.push('http://assets.inhabitat.com/files/2010/01/interloop-stock.jpg');
-      }
-      if(!this.props.listing.pic2){
-        images.push('http://i.istockimg.com/file_thumbview_approve/88947009/6/stock-photo-88947009-swimming-pool-mit-apartment-houses-in-the-background.jpg');
-      }
-    }
       this.setState({images});
+    } 
+
+    if (this.state.images.length === 0){
+        images.push('http://assets.inhabitat.com/files/2010/01/interloop-stock.jpg');
+        images.push('http://i.istockimg.com/file_thumbview_approve/88947009/6/stock-photo-88947009-swimming-pool-mit-apartment-houses-in-the-background.jpg');
+        this.setState({images})
+    }
   }
   render() {
     return (
