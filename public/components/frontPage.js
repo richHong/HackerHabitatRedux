92f56392e9d7133ec752605284cc451106565416
Richard Hong <rich.hong@gmail.com>
2016-08-30 00:05:53 -0700

    increased font for front page

--- public/components/frontPage.js
+++ public/components/frontPage.js
@@ -19,7 +19,7 @@ class FrontPage extends Component {
         <NavBar />
           <div className="filter">
             <div className="overlay">
-              <h1 className="title">Hacker Habitat</h1>
+              <h1 className="bigTitle">Hacker Habitat</h1>
               <p className="frontPageDescription">The tech industry is growing at a rapid pace, and more people than ever are relocating to take part in a fast paced industry. Hacker Habitat makes it possible for tech-minded people to find hacker-houses and live-work spaces for long and short term stays.</p>
               <br/>
               <p className="frontPageDescription">Create your profile and start browsing!</p>
